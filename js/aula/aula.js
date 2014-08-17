21911bf319d362315e7c74f028dac52ae17288af
rafacianci <rafael.fuzifaru@gmail.com>
2014-08-17 03:51:03 -0300

    update

--- js/aula/aula.js
+++ js/aula/aula.js
@@ -1,9 +1,11 @@
 $(function() {
     $("#bt-editar-aula").on("click", function(e) {
         var fd = new FormData();
-        fd.append("idAula", $("#idAula").attr('value'));
-        fd.append("data", $("#data").attr('value'));
-        fd.append("titulo", $("#titulo").attr("value"));
+        fd.append("idAula", $("#idAula").val());
+        fd.append("data", $("#data").val());
+        fd.append("titulo", $("#titulo").val());
+        fd.append("tipo", "editarAula");
+//        console.log(fd);
         $.ajax({
             url: "/queryAjax.php",
             data: fd,
@@ -12,6 +14,18 @@ $(function() {
             contentType: false   // tell jQuery not to set contentType
         }).success(function(msg) {
             console.log(msg);
+            try {
+                data = JSON.parse(msg);
+                if (data.error) {
+//                    alert(data.error);
+                } else {
+                    window.location.hash = data.redirect;
+                    var link = links.getUrl(data.redirect);
+                    links.getPage(link.url, link.params);
+                }
+            } catch (e) {
+                alert("Erro: " + data);
+            }
         }).error(function(e) {
             console.log("erro:" + e);
         })
